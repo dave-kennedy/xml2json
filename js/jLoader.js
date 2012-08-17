fa9dd04c1d34880684811f1e2f5391325ad0fdb8
davidkennedy85 <davidkennedy85@gmail.com>
2012-08-17 16:23:11 -0600

    First commit

---
+++ js/jLoader.js
@@ -0,0 +1,94 @@
+﻿var jLoader = (function () {
+    'use strict';
+
+    var jloader = {
+        // The options parameter is an object that can have any of the following members:
+        // url: The location of the file on the server; REQUIRED
+        // success: The function to perform if the request is a success; REQUIRED
+        // failure: The function to perform if the request is a failure; default: none
+        // method: The type of request; default: GET
+        // allowCache: True to allow caching or false to disallow; default: false
+        // images: An object of objects - one for each image to display at each phase phase of the AJAX request.
+        //         Each object in the images object must have the following members:
+        //         path: The file path to the image
+        //         attach: The id of an element to attach the image to
+        ajax: function (options) {
+            if (!options.url) {
+                console.error('No url parameter supplied for jLoader.ajax');
+
+                return false;
+            }
+
+            if (!options.success) {
+                console.error('No success parameter supplied for jLoader.ajax');
+
+                return false;
+            }
+
+            if (!options.failure) {
+                options.failure = function () {};
+            }
+
+            if (!options.method) {
+                options.method = 'GET';
+            }
+
+            if (!options.allowCache) {
+                options.url = options.url + '?t=' + Math.random();
+            }
+
+            function attachImage(readyState) {
+                if (!options.images) {
+                    return false;
+                }
+
+                if (!options.images[readyState]) {
+                    return false;
+                }
+
+                document.getElementById(options.images[readyState].attach).innerHTML += '<img src="' + options.images[readyState].path + '" />';
+            }
+
+            var httpRequest;
+
+            try {
+                httpRequest = new XMLHttpRequest();
+                attachImage('unsent');
+
+                httpRequest.open(options.method, options.url, true);
+                attachImage('opened');
+
+                httpRequest.send();
+            } catch (err) {
+                options.failure(err);
+                attachImage('failure');
+
+                return false;
+            }
+
+            httpRequest.onreadystatechange = function () {
+                if (httpRequest.status === 404) {
+                    options.failure('The requested URI was not found: ' + options.url);
+                    attachImage('failure');
+
+                    this.onreadystatechange = null;
+
+                    return false;
+                }
+
+                if (httpRequest.readyState === 2) {
+                    attachImage('received');
+                } else if (httpRequest.readyState === 3) {
+                    attachImage('loading');
+                } else if (httpRequest.readyState === 4) {
+                    options.success(httpRequest.responseText);
+                    attachImage('success');
+
+                    return true;
+                }
+            };
+        }
+    };
+
+    return jloader;
+}());
---
+++ js/xml2json.js
@@ -0,0 +1,58 @@
+﻿var xml2json = (function () {
+    // ECMAScript 5 strict mode
+    'use strict';
+
+    var module = {
+        addNode: function (node, obj) {
+            var i, obj = obj || {};
+
+            if (node.attributes.length) {
+                for (i = 0; i < node.attributes.length; i++) {
+                    obj['_' + node.attributes[i].nodeName] = node.attributes[i].nodeValue;
+                }
+            }
+
+            if (node.children.length) {
+                for (i = 0; i < node.children.length; i++) {
+                    obj = this.domToObj(node.children[i], obj);
+                }
+            } else {
+                obj = node.textContent;
+            }
+
+            return obj;
+        },
+        domToObj: function (node, obj) {
+            var i, j;
+
+            if (obj[node.nodeName] instanceof Array) {
+                j = obj[node.nodeName].length;
+
+                obj[node.nodeName][j] = this.addNode(node, obj[node.nodeName][j]);
+            } else if (obj[node.nodeName]) {
+                obj[node.nodeName] = [obj[node.nodeName]];
+
+                obj[node.nodeName][1] = this.addNode(node, obj[node.nodeName][1]);
+            } else {
+                obj[node.nodeName] = this.addNode(node, obj[node.nodeName]);
+            }
+
+            return obj;
+        },
+        parse: function (xml) {
+            var json = {},
+                parser = new DOMParser(),
+                dom = parser.parseFromString(xml, 'text/xml');
+
+            if (dom.nodeType === 9) {
+                dom = dom.documentElement;
+            }
+
+            json = this.domToObj(dom, json);
+
+            return json;
+        }
+    };
+
+    return module;
+}());
